813e5d7a02ef806f1e6725e5b3c6c4d48acd77ae
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2021-05-25 00:14:45 +0200

    add

---
+++ C# Fundamentals/into to C#/intro to CSharp/intro to CSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace intro_to_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment 1
+
+            List<double> li = new List<double>();
+            int num;
+            Console.WriteLine("Enter Number of grades :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0.0;
+            for (int i = 0; i < num; i++)
+            {
+                double number;
+                number = Convert.ToDouble(Console.ReadLine());
+                li.Add(num);
+                result += number;
+            }
+            Console.WriteLine($"Sum of Grade :{result}");
+            Console.WriteLine($"Average of Grades :{result/li.Count}");
+
+
+
+        }
+    }
+}
