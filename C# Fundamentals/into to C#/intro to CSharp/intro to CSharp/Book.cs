72c2da4e0ac50b0f7ff5c976225a03cc68194767
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2021-05-25 18:05:20 +0200

    add

---
+++ C# Fundamentals/into to C#/intro to CSharp/intro to CSharp/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace intro_to_CSharp
+{
+    class Book
+    {
+        private List<double> li;
+        private string name;
+        public Book(string name)
+        {
+            this.name = name;
+            li = new List<double>();
+        }
+        public void AddGrade(double grade)
+        {
+            li.Add(grade);
+        }
+        public void ShowStatistics()
+        {
+          
+            var Sum = 0.0;
+            var LowGrade = double.MaxValue;
+            var hightGrade = double.MinValue;
+
+            foreach (var item in li)
+            {
+                Sum += item;
+                LowGrade = Math.Min(LowGrade, item);
+                hightGrade = Math.Max(hightGrade, item);
+
+            }
+            Console.WriteLine($"Avg : {Sum/li.Count}");
+            Console.WriteLine($"Lowest Value : {LowGrade}");
+            Console.WriteLine($"Highest Value : {hightGrade}");
+        }
+    }
+
+}
--- C# Fundamentals/into to C#/intro to CSharp/intro to CSharp/Program.cs
+++ C# Fundamentals/into to C#/intro to CSharp/intro to CSharp/Program.cs
@@ -2,27 +2,41 @@
 using System.Collections.Generic;
 namespace intro_to_CSharp
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            //Assignment 1
 
-            List<double> li = new List<double>();
-            int num;
-            Console.WriteLine("Enter Number of grades :");
-            num = Convert.ToInt32(Console.ReadLine());
+            #region Assignment 1
+            //List<double> li = new List<double>();
+            //int num;
+            //Console.WriteLine("Enter Number of grades :");
+            //num = Convert.ToInt32(Console.ReadLine());
 
-            double result = 0.0;
-            for (int i = 0; i < num; i++)
-            {
-                double number;
-                number = Convert.ToDouble(Console.ReadLine());
-                li.Add(num);
-                result += number;
-            }
-            Console.WriteLine($"Sum of Grade :{result}");
-            Console.WriteLine($"Average of Grades :{result/li.Count}");
+            //double result = 0.0;
+            //for (int i = 0; i < num; i++)
+            //{
+            //    double number;
+            //    number = Convert.ToDouble(Console.ReadLine());
+            //    li.Add(num);
+            //    result += number;
+            //}
+            //Console.WriteLine($"Sum of Grade :{result}");
+            //Console.WriteLine($"Average of Grades :{result/li.Count}");
+            #endregion
+
+            #region Assignment 2
+
+            //Book b = new Book("mahmoud CS");
+            //b.AddGrade(55.4);
+            //b.AddGrade(99.1);
+            //b.AddGrade(45.0);
+            //b.ShowStatistics();
+
+            #endregion
+
+
 
 
 
