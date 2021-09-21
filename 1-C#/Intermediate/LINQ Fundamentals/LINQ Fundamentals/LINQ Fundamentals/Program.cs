7aa25465c7e94de4ab5bf2e958cc5f270af398c2
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-09-21 22:54:05 +0200

    LINQ Fundamentals

---
+++ 1-C#/Intermediate/LINQ Fundamentals/LINQ Fundamentals/LINQ Fundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LINQ_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
