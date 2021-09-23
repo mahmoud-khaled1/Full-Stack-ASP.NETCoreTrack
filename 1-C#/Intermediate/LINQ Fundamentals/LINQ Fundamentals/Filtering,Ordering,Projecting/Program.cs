82fe2d7b21d9cd305210089c21b865b859191e7b
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-09-23 21:41:02 +0200

    add

--- 1-C#/Intermediate/LINQ Fundamentals/LINQ Fundamentals/Filtering,Ordering,Projecting/Program.cs
+++ 1-C#/Intermediate/LINQ Fundamentals/LINQ Fundamentals/Filtering,Ordering,Projecting/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Cars;
 
 namespace Filtering_Ordering_Projecting
 {
@@ -6,7 +12,34 @@ namespace Filtering_Ordering_Projecting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = ProcessFile("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Manufacturer);
+            }
+          
         }
+
+        private static List<Car> ProcessFile(string path)
+        {
+           // Operator syntax
+           return
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv) //Named Function
+                .ToList();
+
+
+            // Query syntax
+            //var query =
+            //    from Line in File.ReadAllLines(path).Skip(1)
+            //    where Line.Length > 1
+            //    select Car.ParseFromCsv(Line);
+
+            //return query.ToList();
+        }
+
+        
     }
 }
