70d3cd92382aa21a8fbd22b96bb96a4d58920298
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-09-23 23:27:25 +0200

    add

--- 1-C#/Intermediate/LINQ Fundamentals/LINQ Fundamentals/Filtering,Ordering,Projecting/Program.cs
+++ 1-C#/Intermediate/LINQ Fundamentals/LINQ Fundamentals/Filtering,Ordering,Projecting/Program.cs
@@ -13,11 +13,37 @@ namespace Filtering_Ordering_Projecting
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            foreach (var car in cars)
+            //Q1-Get the car that has the best fuel efficiency and if we have more 
+            // than car have same fuel then sort by name ;
+
+            // Operator syntax
+            var query1 = cars.OrderByDescending(c => c.Combined)
+                .ThenByDescending(c=>c.Name).FirstOrDefault();
+
+            Console.WriteLine(query1.Name+" : "+query1.Combined);
+
+            // Query syntax
+            var query2 = (from car in cars
+                         orderby car.Combined descending, car.Name descending
+                         select car).FirstOrDefault();
+            Console.WriteLine(query2.Name+" : "+query2.Combined);
+
+            //Q2-Get all cars that manufactuer is  BMW
+
+            // Operator syntax
+            var query3 = cars.Where(c => c.Manufacturer == "BMW").ToList();
+            foreach (var car in query3)
             {
-                Console.WriteLine(car.Manufacturer);
+                Console.WriteLine(car.Name+" : "+car.Manufacturer);
             }
-          
+            // Query syntax
+            var query4 = (from car in cars
+                         where car.Manufacturer == "BMW"
+                         select car).ToList();
+            foreach (var car in query4)
+            {
+                Console.WriteLine(car.Name + " : " + car.Manufacturer);
+            }
         }
 
         private static List<Car> ProcessFile(string path)
