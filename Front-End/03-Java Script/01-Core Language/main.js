97349a0aa11b39b36ec1f6e4b776cadc85db82da
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-10-21 23:24:56 +0200

    Loops in JS

--- Front-End/03-Java Script/01-Core Language/main.js
+++ Front-End/03-Java Script/01-Core Language/main.js
@@ -6,23 +6,23 @@ document.write("<h3 style=color:red;>Hello from JS File !!</h3>");
 
 console.log("Hello From Js File to Console !");
 
-console.error("Error From Js file to Console !")
+console.error("Error From Js file to Console !");
 
-console.table(["mahmoud","khaled","mohamed"]);
+console.table(["mahmoud", "khaled", "mohamed"]);
 
-console.log("Hello again from %cJs file to Console !","color:red;")
+console.log("Hello again from %cJs file to Console !", "color:red;");
 
 //------------------------------------------------------------------------------
 
 /*  Data Types  */
 
-console.log( typeof"mahmoud Khaled"); //String
-console.log( typeof (100)); //Number
-console.log( typeof (100.5));//Number
-console.log( typeof [10,5,6,5]);//Array => object
-console.log( typeof {name:"mahmoud",age:23,country:"Egypt"}); //Object
-console.log( typeof true);//boolean
-console.log( typeof null); //object
+console.log(typeof "mahmoud Khaled"); //String
+console.log(typeof 100); //Number
+console.log(typeof 100.5); //Number
+console.log(typeof [10, 5, 6, 5]); //Array => object
+console.log(typeof { name: "mahmoud", age: 23, country: "Egypt" }); //Object
+console.log(typeof true); //boolean
+console.log(typeof null); //object
 //------------------------------------------------------------------------------
 
 /*  Variables
@@ -48,67 +48,69 @@ console.log( typeof null); //object
   - Block Or Scope Function
 */
 
-const pi = 3.14
+const pi = 3.14;
 
-pi = 1
+pi = 1;
 //cannot do this becuase with const you cannot change the value
 
 //___________________
 
 //Let --> is block level
 
-for(let i = 0; i < 3; i++) {
-console.log(i) //--> it will console here
+for (let i = 0; i < 3; i++) {
+  console.log(i); //--> it will console here
 }
-console.log(i)// ---> Not here
+console.log(i); // ---> Not here
 
 //----------------------
 
 //Var is for variables available to the entire function
 
-for(var j = 0; j < 3; j++) {
-console.log(j)// --> it will console here
+for (var j = 0; j < 3; j++) {
+  console.log(j); // --> it will console here
 }
-console.log(j) //---> it will console here
+console.log(j); //---> it will console here
 
 //------------------------------------------------------------------------------
 
 /*String Syntax And Character Escape Sequence*/
 
 console.log("mahmoud"); //valid
-console.log('mahmoud'); //valid
-console.log('mahmoud 'khaled''); //invalid
+console.log("mahmoud"); //valid
+//console.log('mahmoud 'khaled''); //invalid
 console.log("mahmoud 'khaled'"); //valid
 console.log('mahmoud "khaled"'); //valid
-console.log('mahmoud \'khaled\''); //valid
+console.log("mahmoud 'khaled'"); //valid
 console.log("mahmoud \
 khaeld \
 mohamed");
 console.log("What is your name \n mahmoud ?"); //(\n) new line
-var namee="mahmoud";
-var lastName="khaled";
-console.log(namee+" "+lastName); //Concatenation
+var namee = "mahmoud";
+var lastName = "khaled";
+console.log(namee + " " + lastName); //Concatenation
 console.log(`${namee} ${lastName}`); //Concatenation also
 
-let markUp=`
+let markUp = `
 <div class="card">
     <div class="child">
         <h2>${namee}</h2>
         <p>${lastName}</p>
     </div>
 </div>
-;`
+;`;
 document.write(markUp);
 
-let ti="ElAero",de="ElZero Web School",da="25/10";
+let ti = "ElAero",
+  de = "ElZero Web School",
+  da = "25/10";
 
-var markUp=`
+var markUp = `
 <div>
     <h3>${ti}</h3>
     <p>${de}</p>
     <span>${da}</span>
 </div>
-`
+`;
 document.write(markUp.repeat(4)); // repeat it 4 time
 
 //------------------------------------------------------------------------------
@@ -123,81 +125,81 @@ console.log(typeof +"100"); //number
 console.log(typeof +"0xff"); //number
 console.log(+"0xff"); //255
 console.log(+true); // 1
-console.log(+false);//0
+console.log(+false); //0
 console.log(true); //true
 console.log(typeof -"100"); //number
 console.log(-"-100"); //100
 console.log(Number("88")); //88  , convert string to number
-console.log(false-true);//0-1=-1
-console.log(20+true);//21
+console.log(false - true); //0-1=-1
+console.log(20 + true); //21
 //------------------------------------------------------------------------------
 
 /* Number */
 
-console.log(1000000);//1000000
-console.log(1_000_000)//1000000
-console.log(1e6);//1000000
-console.log(10**6); //1000000
-console.log(2**2);//4
-console.log(Math.pow(2,3));//8
+console.log(1000000); //1000000
+console.log(1_000_000); //1000000
+console.log(1e6); //1000000
+console.log(10 ** 6); //1000000
+console.log(2 ** 2); //4
+console.log(Math.pow(2, 3)); //8
 console.log(Number.MAX_SAFE_INTEGER); // max safe number you can use .
 
 console.log(typeof (100).toString()); //string
-console.log(typeof 100..toString()); //string
+console.log(typeof (100).toString()); //string
 
-console.log(100.555.toFixed(1)); //100.6 (as string)
+console.log((100.555).toFixed(1)); //100.6 (as string)
 
 console.log(typeof parseInt("120")); // number
-console.log( parseInt("120 mahmoud ")); // 120
+console.log(parseInt("120 mahmoud ")); // 120
 
-console.log(Number.isInteger(100));// true
+console.log(Number.isInteger(100)); // true
 //------------------------------------------------------------------------------
 
 /* Math  */
 
 console.log(Math.round(99.2)); //99
 console.log(Math.ceil(99.2)); //100
-console.log(Math.floor(99.2));//99
-console.log(Math.max(1,25,88,9,9,9,6,66244545,-58));//66244545
-console.log(Math.min(1,25,88,9,9,9,6,66244545,-58));//-58
-console.log(Math.pow(2,4));//16
-console.log(Math.random());// get random number
-console.log(Math.trunc(99.2));//99 get number without ant floating points
+console.log(Math.floor(99.2)); //99
+console.log(Math.max(1, 25, 88, 9, 9, 9, 6, 66244545, -58)); //66244545
+console.log(Math.min(1, 25, 88, 9, 9, 9, 6, 66244545, -58)); //-58
+console.log(Math.pow(2, 4)); //16
+console.log(Math.random()); // get random number
+console.log(Math.trunc(99.2)); //99 get number without ant floating points
 //------------------------------------------------------------------------------
 
 /* String Methods */
 
-let arr=[1,5,7,8];
-let str="mahmoud";
+let arr = [1, 5, 7, 8];
+let str = "mahmoud";
 
 console.log(str[0]); //m
-console.log(arr);//1 5 7 8
-console.log(arr[1]);//5
-console.log(str.charAt(1));//a
-console.log(str.length);//7
-console.log(str.toUpperCase());//MAHMOUD
+console.log(arr); //1 5 7 8
+console.log(arr[1]); //5
+console.log(str.charAt(1)); //a
+console.log(str.length); //7
+console.log(str.toUpperCase()); //MAHMOUD
 
-let str2="  mahmoud  ";
-console.log(str2.trim());//mahmoud  (Remove Spaces from first and end of string )
+let str2 = "  mahmoud  ";
+console.log(str2.trim()); //mahmoud  (Remove Spaces from first and end of string )
 console.log(str2.trim().charAt(3).toUpperCase()); //M ==>remove spaces and make char at index 3 is upper case
-console.log(str.indexOf('d')); // 6 ==> if found it return index if not return -1
-console.log(str.indexOf('r'));// -1
-console.log(str.indexOf("mo"))//3
-console.log(str.indexOf('a',4))//-1 ==> start from index 4 to search about char a
-console.log(str.lastIndexOf('m')); // 3 ==> if found it return index of last m in string  if not return -1
-console.log(str.slice(2,4));//hm
-console.log(str.slice(-2));//ud
-console.log(str.repeat(3));//mahmoudmahmoudmahmoud
+console.log(str.indexOf("d")); // 6 ==> if found it return index if not return -1
+console.log(str.indexOf("r")); // -1
+console.log(str.indexOf("mo")); //3
+console.log(str.indexOf("a", 4)); //-1 ==> start from index 4 to search about char a
+console.log(str.lastIndexOf("m")); // 3 ==> if found it return index of last m in string  if not return -1
+console.log(str.slice(2, 4)); //hm
+console.log(str.slice(-2)); //ud
+console.log(str.repeat(3)); //mahmoudmahmoudmahmoud
 
-let str3="mahmoud,khaled,mohamed";
-console.log(str3.split(','));//(3) ['mahmoud', 'khaled', 'mohamed']
+let str3 = "mahmoud,khaled,mohamed";
+console.log(str3.split(",")); //(3) ['mahmoud', 'khaled', 'mohamed']
 
-console.log(str.substring(2,5));//hmo
-console.log(str.substr(0,2));//ma
-console.log(str.includes("mah"));//true
-console.log(str.startsWith("mah"));//true
-console.log(str.startsWith("mah",2));//false ==> check if from index 2 if string is start with mah
-console.log(str.endsWith("mah"));//false
+console.log(str.substring(2, 5)); //hmo
+console.log(str.substr(0, 2)); //ma
+console.log(str.includes("mah")); //true
+console.log(str.startsWith("mah")); //true
+console.log(str.startsWith("mah", 2)); //false ==> check if from index 2 if string is start with mah
+console.log(str.endsWith("mah")); //false
 //------------------------------------------------------------------------------
 
 /* Comparison Operators  */
@@ -216,14 +218,14 @@ console.log(str.endsWith("mah"));//false
   <= Smaller than or equal
 */
 
-console.log(10=="10");//true ==>compare value only
-console.log(10!="10");//false ==>compare value only
+console.log(10 == "10"); //true ==>compare value only
+console.log(10 != "10"); //false ==>compare value only
 
-console.log(10===10);//true ==> compare value and type
-console.log(10==="10");//false ==> compare value and type
-console.log(10!=="10");//true  ==> compare value and type
+console.log(10 === 10); //true ==> compare value and type
+console.log(10 === "10"); //false ==> compare value and type
+console.log(10 !== "10"); //true  ==> compare value and type
 
-console.log(typeof"mahmoud"  == typeof"khaled");//True
+console.log(typeof "mahmoud" == typeof "khaled"); //True
 //------------------------------------------------------------------------------
 
 /* Comparison Operators  */
@@ -236,13 +238,13 @@ console.log(typeof"mahmoud"  == typeof"khaled");//True
 
 console.log(true); //true
 console.log(!true); // false
-console.log(!(10=="10"));//false
+console.log(!(10 == "10")); //false
 
-console.log((10==10)&&(10=="10"));//true
-console.log((10==10)&&(10==="10"));//false
+console.log(10 == 10 && 10 == "10"); //true
+console.log(10 == 10 && 10 === "10"); //false
 
-console.log((10==10)||(10==="10"));//true
-console.log((10==10)||(10=="10"));//true
+console.log(10 == 10 || 10 === "10"); //true
+console.log(10 == 10 || 10 == "10"); //true
 //------------------------------------------------------------------------------
 
 /*  If Conditions */
@@ -306,7 +308,7 @@ let price;
 
 console.log(price); //undefined
 console.log(price || 200); //200 ==> if price is null or undefined or 0 or false or empty string then will print 200.
-console.log(price??200);//200 ==> if price is null or undefined then print 200.
+console.log(price ?? 200); //200 ==> if price is null or undefined then print 200.
 //------------------------------------------------------------------------------
 
 /* Array */
@@ -389,3 +391,75 @@ console.log(concatArray); //(6) ['aly', 'ahemd', 'mohamed', 'mahmoud', 'khaled'
 console.log(concatArray.join("|")); //aly|ahemd|mohamed|mahmoud|khaled|Moahemd
 
 // //------------------------------------------------------------------------------
+
+/* Loop */
+
+let arr = [1, 98, 7, 6, 5, 4];
+
+//For Loop
+for (let index = 0; index < arr.length; index++) {
+  console.log(arr[index]);
+}
+
+let arr2 = ["asd", 1, "Mahmoud", "Aly", 87, 54];
+let OnlyNames = [];
+for (let index = 0; index < arr2.length; index++) {
+  if (typeof arr2[index] === "string") {
+    OnlyNames.push(arr2[index]);
+  }
+}
+console.log(OnlyNames); //(3) ['asd', 'Mahmoud', 'Aly']
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor"];
+let colors = ["Red", "Green", "Black"];
+let models = [2020, 2021];
+
+for (let i = 0; i < products.length; i++) {
+  console.log("#".repeat(15));
+  console.log(`# ${products[i]}`);
+  console.log("#".repeat(15));
+  console.log("Colors: ");
+  for (let j = 0; j < colors.length; j++) {
+    console.log(`- ${colors[j]}`);
+  }
+  console.log("Models: ");
+  for (let k = 0; k < models.length; k++) {
+    console.log(`- ${models[k]}`);
+  }
+}
+
+mainLoop: for (let i = 0; i < products.length; i++) {
+  console.log(products[i]);
+  nestedLoop: for (let j = 0; j < colors.length; j++) {
+    console.log(`- ${colors[j]}`);
+    if (colors[j] === "Green") {
+      break mainLoop;
+    }
+  }
+}
+
+let showCount = 5;
+document.write(
+  `<h1>show <label style="color:red;">${showCount}</label> Producs</h1>`
+);
+for (let i = 0; i < showCount; i++) {
+  document.write(`<h3>[${i + 1}] ${products[i]}</h3>`);
+  for (let j = 0; j < colors.length; j++) {
+    document.write(`<p>${colors[j]}</p>`);
+  }
+  document.write(`<p>${colors.join(" | ")}</p>`);
+}
+
+//While Loop
+let index = 0;
+while (index < products.length) {
+  console.log(products[index]);
+  index += 1;
+}
+
+//do While Loop
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < products.length);
